8e6944af45343222a58df3bc0426b138e7c28b3d
yogeswar <yogi2198@gmail.com>
2022-03-27 21:44:21 -0400

    added skills to authors

--- common/author.py
+++ common/author.py
@@ -1,8 +1,22 @@
 class Author(object):
     count = 0
-    def __init__(self, id, name):
+
+    def __init__(self, name, id,fos):
         Author.count += 1
         self.id = id
         self.name = name
+        self.fos = fos
         self.teams = set()
-        self.skills = set()
+        self.skills = self.set_skills()
+
+    def set_author_popularity(self):
+        return self.id
+
+    def set_skills(self):
+        skills = set()
+        for skill in self.fos:
+            skills.add(skill["name"].replace(" ", "_"))
+        return skills
+
+    def get_skills(self):
+        return self.skills
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from common.author import Author
 import matplotlib.pyplot as plt
 from collections import Counter
 df = pd.read_json('toy.v12.dblp.json')
@@ -9,8 +10,20 @@ for i,index in enumerate(df["authors"]):
     for j in index:
         Author_name.append(j["name"])
         Author_id.append(j["id"])
-x = Counter(Author_name)
-print(Counter(Author_id))
-x_reverse = dict(sorted(x.items(), key=lambda item: item[1], reverse=True))
-plt.plot(list(Counter(x_reverse).values()))
-plt.show()
+# x = Counter(Author_name)
+# print(Counter(Author_name))
+# x_reverse = dict(sorted(x.items(), key=lambda item: item[1], reverse=True))
+# plt.plot(list(Counter(x_reverse).values()))
+# plt.show()
+author_Instance = []
+for _, index in enumerate(df["authors"]):
+    for author in index:
+        author_Instance.append(Author(author["name"], author["id"],df["fos"][_]))
+
+
+
+
+
+
+
+
