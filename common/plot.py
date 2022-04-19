de9050fffa44c506f40289fed61923b860dce2a3
yogeswar <yogi2198@gmail.com>
2022-04-19 01:57:32 -0400

    code optimisation

--- common/plot.py
+++ common/plot.py
@@ -1,12 +1,7 @@
 from collections import Counter
 import matplotlib.pyplot as plt
-Author_name, Author_id = [], []
-def plot_stats(df):
-    for i, index in enumerate(df["authors"]):
-        for j in index:
-            Author_name.append(j["name"])
-            Author_id.append(j["id"])
-    x = Counter(Author_name)
+def plot_stats(author_id):
+    x = Counter(author_id)
     x_reverse = dict(sorted(x.items(), key=lambda item: item[1], reverse=True))
     plt.plot(list(Counter(x_reverse).values()))
     plt.xlabel("Author")
--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from common.reranking import reranking_logic
 from team_formation import form_teams_with_skills
 from experiment.experiment import experimental_metrics
 df = pd.read_json('toy.v12.dblp.json')
-plot_stats(df)
+# plot_stats(df)
 team_size = 4
 # store list of author classes
 author_Instance =list()
+author_id = list()
 for _, index in enumerate(df["authors"]):
     for author in index:
         author_Instance.append(Author(author["name"], author["id"], df["fos"][_], index))
+        author_id.append(author["id"])
+plot_stats(author_id)
 list_of_skills = ["deep_learning"]
-author_id = list()
-for author in author_Instance:
-    author_id.append(Author.get_author_id(author))
 item_attributes,author_id_to_dictionary = form_teams_with_skills(list_of_skills,author_Instance,author_id)
 author_id_list = list(Counter(author_id_to_dictionary).values())
 item_attributes_reranked,distribution_list = reranking_logic(author_id_list,team_size,len(author_Instance),author_id,author_id_to_dictionary)
