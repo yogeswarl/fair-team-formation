3867bb453542dbd494cc601c8ac95dbbd6631234
yogeswar <yogi2198@gmail.com>
2022-04-22 22:03:10 -0400

    fix to team formation code

--- team_formation.py
+++ team_formation.py
@@ -13,10 +13,10 @@ def convert_author_id_to_attributes(authorList,author_id):
 
 def form_teams_with_skills(list_of_skills, author_Instance,author_id):
 
-    author_list = list()
-    for list in list_of_skills:
+    author_list =list()
+    for list_item in list_of_skills:
         for author in author_Instance:
-            if list in author.get_skills():
+            if list_item in author.get_skills():
                 author_list.append(author.id)
     author_id_to_dictionary = convert_author_id_to_attributes(author_list,author_id)
     return author_list,author_id_to_dictionary
