94bf9da703a1a383ebede84c891f4316026bbc61
yogeswar <yogi2198@gmail.com>
2022-05-03 03:46:38 -0400

    evaluation metrics NDKL and AUC score done

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pickle
+import torch
+import copy
+import pandas as pd
 import numpy as np
+import reranking
+from experiment.metric import *
 import json
 from common.author import Author
 from common.plot import plot_stats
@@ -8,53 +13,42 @@ from common.reranking import reranking_logic
 from team_formation import form_teams_with_skills
 from team_formation import convert_author_id_to_attributes
 from experiment.experiment import experimental_metrics
-print('loading DF')
-author_Instance =list()
-author_id = list()
-# with open('data/toy.v12.dblp.json', "r", encoding='utf-8') as jf:
-#     for line in jf:
-#         try:
-#             if not line: break
-#             jsonline = json.loads(line.lower().lstrip(","))
-#             id = jsonline['id']
-#
-#             try:fos = jsonline['fos']
-#             except:continue
-#             try:authors = jsonline['authors']
-#             except:continue
-#         except json.JSONDecodeError as e:  # ideally should happen only for the last line ']'
-#             print(f'JSONDecodeError: There has been error in loading json line `{line}`!\n{e}')
-#             continue
-#         except Exception as e:
-#             raise e
-#         for author in authors:
-#             author_Instance.append(Author(author["name"],author["id"], fos, authors))
-#             author_id.append(author["id"])
-print('loaded DF')
-team_size = 4
-
-# plot_stats(author_id)
-list_of_skills = ["deep_learning"]
-def load_data(indexes_path,teamsvec_path,list_of_skills):
-    author_id = list()
-    with open(indexes_path, 'rb') as infile:
-        indexes = pickle.load(infile)
-    with open(teamsvec_path, 'rb') as tfile:
-        vecs = pickle.load(tfile)
-
-    list_of_skill_vecs = list()
-    for skill_item in list_of_skills:
-        list_of_skill_vecs.append(indexes['s2i'][skill_item])
-    author_list = list()
-    for r in range(vecs["skill"].shape[0]):
-        for c, d in zip(vecs["skill"].rows[r], vecs["skill"].data[r]):
-            author_id.append(vecs["member"].rows[r])
-            if c in list_of_skill_vecs:
-                author_list.append(vecs["member"].rows[r])
-    return np.concatenate(author_list), np.concatenate(author_id)
-author_list,author_id = load_data(indexes_path='data/processed/dblp-toy/indexes.pkl',teamsvec_path='data/processed/dblp-toy/teamsvecs.pkl',list_of_skills=list_of_skills)
-# item_attributes,author_id_to_dictionary = form_teams_with_skills(list_of_skills,author_Instance,author_list)
-author_id_to_dictionary = convert_author_id_to_attributes(author_id,author_list)
-author_id_list = list(Counter(author_id_to_dictionary).values())
-item_attributes_reranked,distribution_list = reranking_logic(author_id_list,team_size,len(author_id),author_list,author_id_to_dictionary)
-experimental_metrics(author_id_list,item_attributes_reranked,distribution_list)
+vector_data = './data/processed/dblp-toy/teamsvecs.pkl'
+splits = dict()
+with open(vector_data, 'rb') as infile: vecs = pickle.load(infile)
+with open (f'./data/processed/dblp-toy/splits.json','r') as reader:
+  splits = json.load(reader)
+Y = vecs["member"][splits['test']]
+Y_ = torch.load(f'./output/f1.test.pred')
+final_reranked_prediction_list = list()
+prediction_list = [[float(row[i]) for i in range(len(Y_[0]))] for row in Y_]
+prediction_list_copy = copy.deepcopy(prediction_list)
+for listItem in prediction_list:
+  for index,item in enumerate(listItem):
+    if listItem[index] > 0.5: listItem[index] ='P'
+    else: listItem[index]= 'NP'
+distribution_list = {'NP':0.5,'P':0.5}
+result_store = list()
+for counter,row_list,deep_copy in zip(range(len(prediction_list)),prediction_list,prediction_list_copy):
+  ranking_indices= reranking.rerank(
+    row_list,
+    distribution_list,
+    max_na=None,
+    k_max=None,
+    algorithm="det_greedy",
+  )
+  item_attribute_reranked = [row_list[i] for i in ranking_indices]
+  reranked_to_numbers = [deep_copy[i] for i in ranking_indices]
+  final_reranked_prediction_list.append(reranked_to_numbers)
+  before = reranking.ndkl(row_list, distribution_list)
+  after = reranking.ndkl(item_attribute_reranked, distribution_list)
+  np.append(result_store,[before,after])
+  result_store.append([counter,before,after])
+  print(f"The NDKL metric for row-{counter} of before and after re-ranking are {before:.3f} and {after:.3f}, respectively.")
+ndkl_results = pd.DataFrame(result_store)
+final_reranked_prediction_list = np.asarray(final_reranked_prediction_list)
+ndkl_results.to_csv('./output/reranked-ndkl.csv',index=None,header=["row number", "NDKL Before", "NDKL After"])
+# df, df_mean, (fpr, tpr) = calculate_metrics(Y, final_reranked_prediction_list, False)
+auc_score = calculate_metrics(Y, final_reranked_prediction_list, False)
+print(auc_score) #0.6782544378698225
+# df.to_csv(f'./output/test.pred.eval.csv', float_format='%.15f')
